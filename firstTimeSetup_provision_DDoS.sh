92d0c8ebf94fab0bd74bc65cfcf737bb2ed15400
Chris Gray <christophermjgray@gmail.com>
2020-01-15 13:16:14 -0800

    Update firstTimeSetup_provision_DDoS.sh

--- firstTimeSetup_provision_DDoS.sh
+++ firstTimeSetup_provision_DDoS.sh
@@ -1,24 +1,13 @@
 #!/bin/sh
 #
 # By: Christopher Gray
-# Version: 0.0.7
-# Updated: 12/29/19
+# Version: 0.0.8
+# Updated: 1/15/20
 #
 # BigIP Versions Tested on: 15.0 - 15.1
 #
 # Source: https://clouddocs.f5.com/cli/tmsh-reference/latest/modules/net/
-#---- First Time setup of BigIP ----
-echo "Running firstTimeSetup_provision.sh.... "
-wait
-
-#echo "Reset BigIP Config: "
-# tmsh load sys config default
-# sleep 1m
-# reboot
-
 #-----------------------------------------------------------------------------
-# Reminder: After resetting the config. the CLI login is:  root / default
-
 # echo "Resource Provisioning.. "
 # https://techdocs.f5.com/kb/en-us/products/big-ip_ltm/manuals/product/bigip-system-essentials-11-6-0/7.html
 # The module name. The set of valid modules are platform dependent.
@@ -51,20 +40,24 @@ sleep 45
 echo "DDoS  "
 tmsh modify sys provision dos { level nominal }
 sleep 45
+
 # #tmsh sys provision asm { level nominal }
 # #tmsh sys provision ilx { level minimum }
-# tmsh modify sys provision avr { level minimum }
-# sleep 30
 
+
+echo "AVR  "
+tmsh modify sys provision avr { level minimum }
 #-- You should reboot at this point before continuing. --
 #------------------------------------
 echo "Saving config..  "
 tmsh save sys config
-
-echo "Done! Rebooting... "
+wait
+echo "Done!"
 echo "  "
 echo "  "
 echo "Once you log back in, run the script: 'firstTimeSetup.sh' "
 echo "  "
+echo "Reminder: After resetting the config. the CLI login is:  root / default "
 echo "  "
+echo "Rebooting... "
 reboot
