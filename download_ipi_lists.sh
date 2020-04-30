31ce97a704438b63296af712f42367bbaf13b275
Chris Gray <christophermjgray@gmail.com>
2020-04-30 13:16:52 -0400

    Update download_ipi_lists.sh

--- download_ipi_lists.sh
+++ download_ipi_lists.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 Version="0.0.2"
 Updated="4/30/20"
-
 Authors="
 Christopher MJ Gray  | Product Management Engineer - SP | NA   | F5 Networks |       | cgray@f5.com     | https://github.com/c2theg/F5_DDoS_BP
 "
-
+echo "Downloading all lists... "
 curl -o "fullbogons-ipv4.txt" "https://raw.githubusercontent.com/c2theg/DDoS_lists/master/fullbogons-ipv4.txt"
 curl -o "fullbogons-ipv6.txt" "https://raw.githubusercontent.com/c2theg/DDoS_lists/master/fullbogons-ipv6.txt"
 curl -o "Tor_exit_nodes.txt" "https://raw.githubusercontent.com/c2theg/DDoS_lists/master/Tor_exit_nodes.txt"
